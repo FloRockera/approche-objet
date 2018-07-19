a7c15663ff07d3327383c73e9381da6476003942
Flora Calbris <flora.calbris@gmail.com>
2018-07-19 14:43:41 +0200

    TP6.2

--- src/zoo/Animal.java
+++ src/zoo/Animal.java
@@ -12,6 +12,39 @@ public class Animal {
 		type = nvtype;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+
+
 	//Getters et setters
 	public String getNom() {
 		return nom;
--- src/zoo/GestionZoo.java
+++ src/zoo/GestionZoo.java
@@ -5,18 +5,22 @@ public class GestionZoo {
 	public static void main(String[] args){
 
 		Zoo zoo1 = new Zoo("Beauval");
-		
+
 		//1ere maniere d'instancier zoo
 		Zone zoneCarnivore = zoo1.getZoneCar();
 		zoneCarnivore.addAnimal(new Animal("toto","Taureau"));
 		zoneCarnivore.addAnimal(new Animal("kiki","Lion"));
-		
+
 		//2e maniere d'instancier zoo BONNE PRATIQUE
 		zoo1.getZoneCar().addAnimal(new Animal("toto","Taureau"));
 		zoo1.getZoneCar().addAnimal(new Animal("kiki","Lion"));
 
 		//Compter le nombre d'animaux présents dans le zoo
 		System.out.println(zoo1.getZoneCar().getNombre());	
+
+		Animal an1 = new Animal("toto","Taureau");
+		Animal an2 = new Animal("toto","Taureau");
+		System.out.println(an1.equals(an2));
 
 	}
 }
