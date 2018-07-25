8534029d15e03ab1279a913573a121acda57ec58
Flora Calbris <flora.calbris@gmail.com>
2018-07-25 16:18:24 +0200

    Exercices d'invocation de méthodes

--- src/exercices/TestSolution.java
+++ src/exercices/TestSolution.java
@@ -5,6 +5,24 @@ public class TestSolution {
 	public static void main(String[] args) {
 		
 
+	Boolean a = Solution.isFunny("hello");
+	System.out.println(a);
+	
+	String b = Solution.reverse("coucou");
+	System.out.println(b);
+	
+	Solution c = new Solution();
+	int result = c.costOfSubstitution('a','b');
+	System.out.println(result);
+	
+	Solution d = new Solution();
+	int i = d.min(123);
+	System.out.println(i);
+	
+	Solution m = new Solution();
+	int distance = m.levenshtein("12", "5");
+	System.out.println(distance);
+	
 	}
 
 }
